329932786ed8d81ea50d0f6e38eb8fcc0dd3e417
Grant <javaughngrant28@gmail.com>
2024-01-03 19:42:07 -0500

    Added comments folder and created TopBar commponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
-
+import React from "react";
+import TopBar from './components/TopBar'
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <TopBar/>
   );
 }
 
