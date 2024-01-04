f87b780ff055cb6a823c31f07588650b3667ee34
Grant <javaughngrant28@gmail.com>
2024-01-04 17:53:32 -0500

    NavBar commonent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import TopBar from './components/TopBar'
+import { NavBar } from "./components/NavBar";
+
 function App() {
   return (
+    <>
     <TopBar/>
+    <NavBar/>
+    </>
   );
 }
 
