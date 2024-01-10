65164973219c3a8858cf5cfdf54ececf5cc7d08a
Grant <javaughngrant28@gmail.com>
2024-01-10 14:02:17 -0500

    Activity card components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import TopBar from './components/TopBar'
 import NavBar from "./components/NavBar";
 import  Hero  from "./components/Hero";
+import Activities from "./components/Activities";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <TopBar/>
     <NavBar/>
     <Hero/>
+    <Activities/>
     </>
   );
 }
