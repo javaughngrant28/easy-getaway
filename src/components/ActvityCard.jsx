646a19eb482e92652f814e20452fb88a18d45e1e
Grant <javaughngrant28@gmail.com>
2024-01-10 14:16:57 -0500

    Activity card title prop

--- src/components/ActvityCard.jsx
+++ src/components/ActvityCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function ActvityCard({image}) {
+function ActvityCard({image,title}) {
   return (
     <div className='relative p-4'>
-        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>{title}</h3>
         <img
         className='w-full h-full object-cover relative border-4 border-white shadow-lg'
           src= {image}
